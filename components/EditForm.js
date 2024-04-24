a2eece09f89e3b96ff60e29c5eb53b89149068e9
stharavi01 <sanayashe@gmail.com>
2024-04-24 11:49:01 +0545

    complete edit form

--- components/EditForm.js
+++ components/EditForm.js
@@ -1,7 +1,24 @@
+'use client';
+import { editTask } from "@/utils/actions";
+
 const EditForm = ({ task }) => {
-    console.log(task);
+    const { id, completed, content } = task;
     return (
-        <div>EditForm</div>
+        <form
+            action={editTask}
+            className="max-w-sm p-12 border border-base-300 rounded-lg">
+            <input type="hidden" name="id" value={id} />
+            {/* content */}
+            <input type="text" required defaultValue={content} name="content" className="input input-bordered w-full" />
+            {/* completed */}
+            <div className="form-control my-4 ">
+                <label htmlFor="completed" className="label cursor-pointer">
+                    <span className="label-text">Completed</span>
+                    <input type="checkbox" defaultChecked={completed} id="completed" name="completed" className="checkbox checkbox-primary checkbox-sm" />
+                </label>
+            </div>
+            <button type="submit" className="btn btn-primary btn-block btn-sm">Edit</button>
+        </form>
     );
 };
 export default EditForm;
